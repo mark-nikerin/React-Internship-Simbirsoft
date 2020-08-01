965dcb33375a4883e485b6f84fb171e5a6fe62d3
mark-nikerin <mrnikerin@gmail.com>
2020-08-01 21:21:53 +0400

    V-01 Add link to maps and order page

--- need-for-drive-app/src/components/Main/Main.js
+++ need-for-drive-app/src/components/Main/Main.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import "./main.css"
-import icons from "../../assets/icons.svg"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./main.css";
+import icons from "../../assets/icons.svg";
 
 const Main = () => {
-  return(
+  return (
     <div className="main">
       <div className="main__header">
         <h1 className="unselectable">Need for drive</h1>
@@ -11,16 +12,18 @@ const Main = () => {
           <svg className="location__icon" width="18" height="20">
             <use xlinkHref={`${icons}#location`}></use>
           </svg>
-          <span>Ульяновск</span>
-        </div> 
+          <span><a href="https://yandex.ru/maps/?ll=48.482301,54.300849&z=12&l=map">Ульяновск</a></span>
+        </div>
       </div>
       <div className="main__content">
         <h1 className="unselectable">Каршеринг</h1>
-        <h2 className="unselectable" >Need for drive</h2>
-        <h3 className="unselectable" >Поминутная аренда авто твоего города</h3>
-        <button className="button">
-          <span>Забронировать</span>
-        </button>
+        <h2 className="unselectable">Need for drive</h2>
+        <h3 className="unselectable">Поминутная аренда авто твоего города</h3>
+        <Link to="/React-Internship-Simbirsoft/order">
+          <button className="button">
+            <span>Забронировать</span>
+          </button>
+        </Link>
       </div>
       <div className="main__footer">
         <span>© 2016-2019 «Need for drive»</span>
@@ -28,6 +31,6 @@ const Main = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Main;
