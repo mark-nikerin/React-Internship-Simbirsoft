e9798685b22c8e62060da82fbcc795b4b9f45e1b
mark-nikerin <mrnikerin@gmail.com>
2020-08-03 01:58:15 +0400

    V-02 Add OrderInfo component with style

---
+++ need-for-drive-app/src/components/OrderInfo/OrderInfo.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./orderInfo.css"
+
+const OrderInfo = () => {
+  return(
+    <div className="order-info">
+      <h1>Ваш заказ:</h1>
+      <ul className="info">
+        <li className="info__item">
+          <h4>Пункт выдачи</h4>
+          <span>Ульяновск, Нариманова 42</span>
+        </li> 
+      </ul> 
+      <div className="price">
+        <h3>Цена:</h3>
+        <span>От 8000 до 12000 Р</span>
+      </div>
+      <button className="button inactive"><span>Выбрать модель</span></button>
+    </div>
+  )
+}
+
+export default OrderInfo;
--- need-for-drive-app/src/pages/order.js
+++ need-for-drive-app/src/pages/order.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Header from "../components/Header";
 import StepsMenu from "../components/StepsMenu";
+import OrderInfo from "../components/OrderInfo";
+import "./order.css";
 
 const OrderPage = () => {
   return (
     <div className="order-page">
       <Header />
       <StepsMenu />
+      <div className="steps-body"> 
+        <div className="step-body"></div>
+        <OrderInfo />
+      </div> 
     </div>
   );
 }
