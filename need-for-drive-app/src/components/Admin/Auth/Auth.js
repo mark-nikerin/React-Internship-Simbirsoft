3a9ca2cd1cffa829444be004702e0c2fc678fca7
mark-nikerin <mrnikerin@gmail.com>
2020-09-23 00:47:35 +0400

    V-07 Add AdminPage, Card and Auth components

---
+++ need-for-drive-app/src/components/Admin/Auth/Auth.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "../Card"
+import "./auth.css";
+
+const Auth = () => {
+  return <Card>
+    <div>Привет как дела?в фы фы фы фы фы фы фы фы фы фы фы фы фы фы фы фы фы фып</div>
+    <div>Нормально</div>
+  </Card>;
+};
+
+export default Auth;
---
+++ need-for-drive-app/src/pages/admin.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Auth from "../components/Admin/Auth";
+
+const AdminPage = () => {
+  return (<div className="admin-page">
+    <Auth></Auth>
+  </div>);
+}
+
+export default AdminPage;
